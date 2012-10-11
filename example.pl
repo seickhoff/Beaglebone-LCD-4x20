7824fd510c4c810caa12ac1796a98fea8c34a913
Scott Eickhoff <seickhoff@cox.net>
2012-10-10 22:39:09 -0500

    Update example.pl

--- example.pl
+++ example.pl
@@ -1,16 +1,28 @@
 require LCD;
 
+# initialize LCD, clear screen and set cursor to upper left
+# you can supply your own configuration in this constructor
+
 $lcd  = LCD->new();
 
-$lcd->configuration;
+# print the gpio configuration; for debugging
 
-$lcd->set_position(0, 0);
-$lcd->print("Beaglebone");
+$lcd->configuration; 
+
+# row, column
+
+$lcd->set_position(0, 0); 
+
+# write string to LCD display
+
+$lcd->print("Beaglebone"); 
 
 $lcd->set_position(2, 0);
 $lcd->print("LCD 4x20 HD77480");
 
-$lcd->close;
+# cleanup
+
+$lcd->close; 
 
 exit;
 
